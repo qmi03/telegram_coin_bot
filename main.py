226847e58a7838f62fc8066f8f9147aeeb1966fa
Minh <minh.pham1126@gmail.com>
2024-06-07 02:09:52 +0700

    feat: added echo message functionality

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (ApplicationBuilder, CommandHandler, ContextTypes,
+                          MessageHandler, filters)
 
 load_dotenv()
 
@@ -22,8 +23,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text
+    )
+
+
 if __name__ == "__main__":
     application = ApplicationBuilder().token(telegram_api_key).build()
     start_handler = CommandHandler("start", start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(start_handler)
+    application.add_handler(echo_handler)
     application.run_polling()
